28f58a2279322d9ef9c284f599f1e1eb527ebabe
DenysSoldatenko <soldatenko911@gmail.com>
2024-02-17 13:45:00 +0300

    Added the option to edit job activity

--- src/main/java/com/example/project/controllers/JobSeekerApplyController.java
+++ src/main/java/com/example/project/controllers/JobSeekerApplyController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,10 +18,18 @@ public class JobSeekerApplyController {
   private final UserService usersService;
 
   @GetMapping("job-details-apply/{id}")
-  public String display(@PathVariable("id") int id, Model model) {
+  public String showJobDetails(@PathVariable("id") int id, Model model) {
     PostActivity jobDetails = postActivityService.findById(id);
     model.addAttribute("jobDetails", jobDetails);
     model.addAttribute("user", usersService.getCurrentUserProfile());
     return "job-details";
+  }
+
+  @PostMapping("dashboard/edit/{id}")
+  public String editJob(@PathVariable("id") int id, Model model) {
+    PostActivity jobPostActivity = postActivityService.findById(id);
+    model.addAttribute("jobPostActivity", jobPostActivity);
+    model.addAttribute("user", usersService.getCurrentUserProfile());
+    return "add-jobs";
   }
 }
